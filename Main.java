989180bccec3eab0d3ec13dbbc9f2255b231ec26
EdgMik <superstarkelevra@gmail.com>
2022-08-18 18:47:47 +0200

    nauka zmiennych w Javie

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Edgar";
-        System.out.println("Hej"+" "+name+"!");
+        //System.out.println("Hej "+name+" !");
+
+        int age = 34;
+        char znak = 'i';
+        int x = 10;
+        int y = 20;
+        int c = x + y;
+        //System.out.println("Nazywam się "+name+" "+znak+ " mam "+age+" lata");
+        System.out.println(c);
+
     }
+
 }
