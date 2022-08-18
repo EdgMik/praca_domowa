9d0143d064dca4041c2baba6e607c5a66b0b8d50
EdgMik <superstarkelevra@gmail.com>
2022-08-18 20:45:54 +0200

    pętla fori i foreach

---
+++ src/SklepZoologiczny.java
@@ -0,0 +1,16 @@
+public class SklepZoologiczny {
+    public static void main(String[] args) {
+        String[] animalName;
+        animalName = new String[]{"tygrys", "słoń", "małpa", "hipopotam", "wąż"};
+
+//        for (int i = 0; i <= 4; i++) {
+//            System.out.println(animalName[i]);
+        for ( String animal:animalName){
+            System.out.println(animal);
+        }
+            
+
+
+
+        }
+    }
